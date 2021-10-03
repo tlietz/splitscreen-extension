fec314dac557e09b487aebea0f85bbfa74a09d51
Theodore Lietz <theodore.lietz@gmail.com>
2021-10-02 22:32:36 -0500

    increased reuse of functions

--- background.js
+++ background.js
@@ -1,9 +1,14 @@
-import { createPopupFromActiveTab, convertWindow } from "./utils/functions.js";
+import { convertWindow, createPopupFromUrl } from "./utils/functions.js";
 
 chrome.runtime.onInstalled.addListener(() => {
     chrome.contextMenus.create({
+        id: "popup-from-link",
+        title: "Popup From Link",
+        contexts: ["link"],
+    });
+    chrome.contextMenus.create({
         id: "create-popup",
-        title: "Create Popup",
+        title: "Popup From Page",
         contexts: ["all"],
     });
     chrome.contextMenus.create({
@@ -11,23 +16,19 @@ chrome.runtime.onInstalled.addListener(() => {
         title: "Convert Normal/Popup",
         contexts: ["all"],
     });
-    chrome.contextMenus.create({
-        id: "popup-from-link",
-        title: "Create Popup From Link",
-        contexts: ["link"],
-    });
 });
 
-chrome.contextMenus.onClicked.addListener(async (menuItemId) => {
-    const id = menuItemId.menuItemId;
+chrome.contextMenus.onClicked.addListener(async (onClickData) => {
+    const id = onClickData.menuItemId;
     if (id === "create-popup") {
-        createPopupFromActiveTab();
+        createPopupFromUrl(onClickData.pageUrl);
     } else if (id === "convert") {
         convertWindow();
     } else if (id === "popup-from-link") {
+        createPopupFromUrl(onClickData.linkUrl);
     }
 });
 
-chrome.action.onClicked.addListener(async () => {
-    createPopupFromActiveTab();
+chrome.action.onClicked.addListener(async (tab) => {
+    ceatePopupFromUrl(tab.url);
 });
